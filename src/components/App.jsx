131c8a7c1fed6933dd4136e783e24ccf326e7d39
shadow-rogue <ritikeshraj444@gmail.com>
2021-04-24 22:20:00 +0530

    Input and delete functionality of notes added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-
+import CreateArea from "./CreateArea";
 
 function App() {
+const [notes, setNotes] = useState([]);
+
+function addNote(note) {
+  setNotes (prevNotes => {
+    return [...prevNotes, note];
+  });
+}
+
+function deleteNote(id) {
+  setNotes (prevNotes => {
+    return prevNotes.filter( (noteItem,index)=> {
+      return index !== id;
+    });
+  });
+}
+
+
   return (
     <div>
       <Header />
-      {notes.map( newNote => {
-    return (<Note 
-        key= {newNote.key}
-        title = {newNote.title}
-        content = {newNote.content}
-    />);
-})}
+      <CreateArea 
+      onAdd = {addNote}
+      />
+      {notes.map((noteItem,index) => {
+        return <Note
+          key = {index}
+          id = {index}
+           title ={noteItem.title} 
+           content = {noteItem.content}  
+           onDelete = {deleteNote}     
+         />;
+      })
+      }
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const date = new Date();
-    var year = date.getFullYear();
-
-    return (
-        <footer>
-         <p>Copyright {year}</p>
-        </footer>
-    )
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
